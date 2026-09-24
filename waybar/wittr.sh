6deec065c1df7526336fc07df07f6dd049408f95
Ricky de Abreu Barbosa <rickybrbsa@protonmail.com>
2026-09-23 23:25:45 -0300

    Delete waybar directory

--- waybar/wittr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-req=$(curl -s wttr.in/?format="%t|%l+(%c%f)+%h,+%C")
-bar=$(echo $req | awk -F "|" '{print $1}')
-tooltip=$(echo $req | awk -F "|" '{print $2}')
-echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
